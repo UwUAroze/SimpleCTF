2b2ba755cc157ff203628db2287db88f05280e6a
Aroze <admin@aroze.me>
2026-03-21 18:06:51 +0000

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    id("java")
+}
+
+group = "me.aroze"
+version = "1.0"
+
+repositories {
+    maven("https://repo.papermc.io/repository/maven-public/")
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.21.11-R0.1-SNAPSHOT")
+}
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
+}
---
+++ src/main/java/me/aroze/simplectf/SimpleCTF.java
@@ -0,0 +1,17 @@
+package me.aroze.simplectf;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SimpleCTF extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        System.out.println("Enabled SimpleCTF");
+    }
+
+    @Override
+    public void onDisable() {
+        System.out.println("Disabled");
+    }
+
+}
